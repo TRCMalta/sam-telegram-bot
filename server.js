120b746a01167420d83112786f666bbca87d1855
TRCMalta <jonathan@theremarkablecollective.com>
2026-04-04 10:50:09 +0200

    feat: add placementdetailsAPI-read scope for Beverly placement queries

--- server.js
+++ server.js
@@ -642,7 +642,7 @@ async function firefishAuth() {
     grant_type: "client_credentials",
     client_id: FIREFISH_CLIENT_ID,
     client_secret: FIREFISH_CLIENT_SECRET,
-    scope: "candidatesAPI-read companiesAPI-read jobsAPI-read actionsAPI-read advertsAPI-read candidatesAPI-write",
+    scope: "candidatesAPI-read companiesAPI-read jobsAPI-read actionsAPI-read advertsAPI-read candidatesAPI-write placementdetailsAPI-read",
   });
 
   const res = await fetchJSON(
@@ -1883,7 +1883,7 @@ app.get("/debug", async (req, res) => {
       client_id: FIREFISH_CLIENT_ID,
       client_secret: FIREFISH_CLIENT_SECRET,
     
-    scope: "candidatesAPI-read companiesAPI-read jobsAPI-read actionsAPI-read advertsAPI-read candidatesAPI-write",});
+    scope: "candidatesAPI-read companiesAPI-read jobsAPI-read actionsAPI-read advertsAPI-read candidatesAPI-write placementdetailsAPI-read",});
     const rawAuth = await fetchJSON("https://api.firefishsoftware.com/authorization/token", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded",
